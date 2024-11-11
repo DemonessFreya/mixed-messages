3bed22c55405ff3db12c175f52df46df230b306f
Freyja <freyamarika@gmail.com>
2024-11-11 15:15:03 +1100

    undefined error logic error fix

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ verb = ['run', 'steal'];
 object = ['loser', 'winner'];
 
 const message = () => {
-    let subjectWord = subject[Math.ceil(Math.random() * (subject.length))];
-    let verbWord = verb[Math.ceil(Math.random() * (verb.length))];
-    let objectWord = object[Math.ceil(Math.random() * (object.length))];
+    let subjectWord = subject[Math.ceil(Math.random() * (subject.length)) - 1];
+    let verbWord = verb[Math.ceil(Math.random() * (verb.length)) - 1];
+    let objectWord = object[Math.ceil(Math.random() * (object.length)) - 1];
 
     console.log(`Hello ${subjectWord}, you ${verbWord} like a ${objectWord}`);
 }
