43c447b32411ef0cfd517986b065f34e06a5a8df
Freyja <freyamarika@gmail.com>
2024-11-11 16:03:24 +1100

    expanded code to include words beginning with vowels

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-subject = ["turtle", "dog", "cat", "bird", "student", "teacher", "car", "house", "tree", "computer", "mountain", "city", "book", "actor", "chef", "artist"];
+subject = ["turtle", "dog", "cat", "bird", "student", "teacher", "car", "house", "tree", "computer", "mountain", "city", "book", "chef", "artist"];
+vowel_subject = ["actor", "apple", "elephant", "idea", "island", "orange", "umbrella", "octopus", "ice", "engine", "omelet"];
+
 verb = ["run", "jump", "swim", "read", "write", "eat", "sleep", "speak", "sing", "dance", "study", "draw", "cook", "drive", "play"];
+
 object = ["ball", "book", "pen", "phone", "computer", "car", "table", "chair", "bag", "bottle", "watch", "key", "hat", "shoe", "lamp"];
+vowel_object = ["apple", "elephant", "ice", "umbrella", "orange", "engine", "oven", "island", "ink", "egg"];
 
 const message = () => {
-    let subjectWord = subject[Math.ceil(Math.random() * (subject.length)) - 1];
-    let verbWord = verb[Math.ceil(Math.random() * (verb.length)) - 1];
-    let objectWord = object[Math.ceil(Math.random() * (object.length)) - 1];
+    choice = Math.ceil(Math.random() * 4)
 
-    console.log(`A(n) ${subjectWord} may ${verbWord} like a ${objectWord}`);
+    if (choice === 1) {
+        let subjectWord = subject[Math.ceil(Math.random() * (subject.length)) - 1];
+        let verbWord = verb[Math.ceil(Math.random() * (verb.length)) - 1];
+        let objectWord = object[Math.ceil(Math.random() * (object.length)) - 1];
+
+        console.log(`A ${subjectWord} may ${verbWord} like a ${objectWord}`);
+    } else if (choice === 2) {
+        let subjectWord = vowel_subject[Math.ceil(Math.random() * (vowel_subject.length)) - 1];
+        let verbWord = verb[Math.ceil(Math.random() * (verb.length)) - 1];
+        let objectWord = object[Math.ceil(Math.random() * (object.length)) - 1];
+
+        console.log(`An ${subjectWord} may ${verbWord} like a ${objectWord}`);
+    } else if (choice === 3) {
+        let subjectWord = subject[Math.ceil(Math.random() * (subject.length)) - 1];
+        let verbWord = verb[Math.ceil(Math.random() * (verb.length)) - 1];
+        let objectWord = vowel_object[Math.ceil(Math.random() * (vowel_object.length)) - 1];
+
+        console.log(`A ${subjectWord} may ${verbWord} like an ${objectWord}`);
+    } else if (choice === 4) {
+        let subjectWord = vowel_subject[Math.ceil(Math.random() * (vowel_subject.length)) - 1];
+        let verbWord = verb[Math.ceil(Math.random() * (verb.length)) - 1];
+        let objectWord = vowel_object[Math.ceil(Math.random() * (vowel_object.length)) - 1];
+
+        console.log(`An ${subjectWord} may ${verbWord} like an ${objectWord}`);
+    }
 }
 
 message();
