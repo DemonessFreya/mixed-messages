8c343f8757e4f4b41220be998766e39a5dedc567
Freyja <freyamarika@gmail.com>
2024-11-11 15:48:46 +1100

    added a number of subjects, verbs, and objects to the three arrays

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-subject = ['turtle', 'pig'];
-verb = ['run', 'steal'];
-object = ['loser', 'winner'];
+subject = ["turtle", "dog", "cat", "bird", "student", "teacher", "car", "house", "tree", "computer", "mountain", "city", "book", "actor", "chef", "artist"];
+verb = ["run", "jump", "swim", "read", "write", "eat", "sleep", "speak", "sing", "dance", "study", "draw", "cook", "drive", "play"];
+object = ["ball", "book", "pen", "phone", "computer", "car", "table", "chair", "bag", "bottle", "watch", "key", "hat", "shoe", "lamp"];
 
 const message = () => {
     let subjectWord = subject[Math.ceil(Math.random() * (subject.length)) - 1];
     let verbWord = verb[Math.ceil(Math.random() * (verb.length)) - 1];
     let objectWord = object[Math.ceil(Math.random() * (object.length)) - 1];
 
-    console.log(`Hello ${subjectWord}, you ${verbWord} like a ${objectWord}`);
+    console.log(`A(n) ${subjectWord} may ${verbWord} like a ${objectWord}`);
 }
 
 message();
